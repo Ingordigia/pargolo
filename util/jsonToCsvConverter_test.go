3968bf07cad04fd1e184250fbbc3f5fa38d86ea2
Luca Persegani <persegani@mailup.com>
2019-08-27 14:42:13 +0200

    removed "onlyEmpty" boolean option because of csv.Writer logic
    csv.Writer automatically adds double quotes if strings contains comma(s) and this conflicts with our upload csv logic

--- util/jsonToCsvConverter_test.go
+++ util/jsonToCsvConverter_test.go
@@ -1,12 +1,12 @@
 package util
 
 import (
-	"github.com/stretchr/testify/assert"
 	"io/ioutil"
 	"strings"
 	"testing"
-)
 
+	"github.com/stretchr/testify/assert"
+)
 
 func TestOneVariableOneLevel(t *testing.T) {
 	//input := `{"Environment": ""}`
@@ -14,24 +14,11 @@ func TestOneVariableOneLevel(t *testing.T) {
 	// check if file is json
 	file, _ := ioutil.ReadFile(inputPath)
 
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, true)
+	actualCsv, err := NewJSONToCsvConverter().Convert(file)
 	if err != nil {
 		t.Fail()
 	}
-	assert.Equal(t, `Environment,string,`, actualCsv[0])
-}
-
-func TestOneVariableOneLevelWithValue(t *testing.T) {
-	//input := `{"Environment": ""}`
-	inputPath := "json/testOneVariableOneLevelWithValue.json"
-	// check if file is json
-	file, _ := ioutil.ReadFile(inputPath)
-
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, false)
-	if err != nil {
-		t.Fail()
-	}
-	assert.Equal(t, `Environment,string,5`, actualCsv[0])
+	assert.Equal(t, `Environment`, actualCsv[0])
 }
 
 func TestOneVariableOneLevelWithValue_ButOnlyEmpty_ShouldReturnNothing(t *testing.T) {
@@ -40,7 +27,7 @@ func TestOneVariableOneLevelWithValue_ButOnlyEmpty_ShouldReturnNothing(t *testin
 	// check if file is json
 	file, _ := ioutil.ReadFile(inputPath)
 
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, true)
+	actualCsv, err := NewJSONToCsvConverter().Convert(file)
 	if err != nil {
 		t.Fail()
 	}
@@ -53,30 +40,13 @@ func TestOneVariableTwoLevel(t *testing.T) {
 	// check if file is json
 	file, _ := ioutil.ReadFile(inputPath)
 
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, true)
+	actualCsv, err := NewJSONToCsvConverter().Convert(file)
 	if err != nil {
 		t.Fail()
 	}
-	expected := `Sentry/SentryDSN,string,`
+	expected := `Sentry/SentryDSN`
 
-	assert.Equal(t, expected , actualCsv[0])
-}
-
-func TestOneVariableTwoLevelAll(t *testing.T) {
-	//input := `{"Sentry": {"SentryDSN": ""}}`
-	inputPath := "json/testOneVariableTwoLevel.json"
-	// check if file is json
-	file, _ := ioutil.ReadFile(inputPath)
-
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, false)
-	if err != nil {
-		t.Fail()
-	}
-
-	actual := strings.Join(actualCsv,"|")
-
-	assert.Equal(t, true, strings.Contains(actual, `Sentry/SentryDSN,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Sentry/ClientID,string,e7a096db-f113-9ca2-b091-2b3f7f4420af`))
+	assert.Equal(t, expected, actualCsv[0])
 }
 
 func TestIfInvalidJsonShouldReturnError(t *testing.T) {
@@ -84,9 +54,9 @@ func TestIfInvalidJsonShouldReturnError(t *testing.T) {
 	// check if file is json
 	file, _ := ioutil.ReadFile(input)
 
-	_, err := NewJsonToCsvConverter().Convert(file, true)
+	_, err := NewJSONToCsvConverter().Convert(file)
 	if err != nil {
-		assert.Equal(t,"unexpected end of JSON input", err.Error())
+		assert.Equal(t, "unexpected end of JSON input", err.Error())
 	}
 }
 
@@ -96,11 +66,11 @@ func TestOneVariableThreeLevel(t *testing.T) {
 	// check if file is json
 	file, _ := ioutil.ReadFile(inputPath)
 
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, true)
+	actualCsv, err := NewJSONToCsvConverter().Convert(file)
 	if err != nil {
 		t.Fail()
 	}
-	expected := `Sentry/SentryDSN1/SentryDSN2,string,`
+	expected := `Sentry/SentryDSN1/SentryDSN2`
 
 	assert.Equal(t, expected, actualCsv[0])
 }
@@ -110,18 +80,18 @@ func TestMultilineOneVariableOneLevel(t *testing.T) {
 	// check if file is json
 	file, _ := ioutil.ReadFile(inputPath)
 
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, true)
+	actualCsv, err := NewJSONToCsvConverter().Convert(file)
 	if err != nil {
 		t.Fail()
 	}
-	actual := strings.Join(actualCsv,"|")
+	actual := strings.Join(actualCsv, "|")
 
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/SecretKey,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/ClientSecret,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Environment,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `WebApp/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Log/RollingFile/File,string,`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/SecretKey`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/ClientSecret`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Endpoint`))
+	assert.Equal(t, true, strings.Contains(actual, `Environment`))
+	assert.Equal(t, true, strings.Contains(actual, `WebApp/Port`))
+	assert.Equal(t, true, strings.Contains(actual, `Log/RollingFile/File`))
 }
 
 func TestMultilineOneVariableSameLevel(t *testing.T) {
@@ -130,14 +100,14 @@ func TestMultilineOneVariableSameLevel(t *testing.T) {
 	// check if file is json
 	file, _ := ioutil.ReadFile(inputPath)
 
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, true)
+	actualCsv, err := NewJSONToCsvConverter().Convert(file)
 	if err != nil {
 		t.Fail()
 	}
-	actual := strings.Join(actualCsv,"|")
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/SecretKey,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/ClientSecret,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Endpoint,string,`))
+	actual := strings.Join(actualCsv, "|")
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/SecretKey`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/ClientSecret`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Endpoint`))
 }
 
 func TestRealCase(t *testing.T) {
@@ -145,66 +115,30 @@ func TestRealCase(t *testing.T) {
 	// check if file is json
 	file, _ := ioutil.ReadFile(inputPath)
 
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, true)
+	actualCsv, err := NewJSONToCsvConverter().Convert(file)
 	if err != nil {
 		t.Fail()
 	}
-	actual := strings.Join(actualCsv,"|")
+	actual := strings.Join(actualCsv, "|")
 
-	assert.Equal(t, true, strings.Contains(actual, `Environment,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `AppClientId,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `WebApp/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Sentry/SentryDSN,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Log/RollingFile/File,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `ConsoleCredentials/Username,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `ConsoleCredentials/Password,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Redis/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Redis/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Redis/Database,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Database,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/SecretKey,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/ClientSecret,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Database,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `ServiceEndpoints/Endpoint1/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `ServiceEndpoints/Endpoint2/Endpoint,string,`))
-}
-
-
-func TestRealCaseNotOnlyEmpty(t *testing.T) {
-	inputPath := "json/testRealCase.json"
-	// check if file is json
-	file, _ := ioutil.ReadFile(inputPath)
-
-	actualCsv, err := NewJsonToCsvConverter().Convert(file, false)
-	if err != nil {
-		t.Fail()
-	}
-	actual := strings.Join(actualCsv,"|")
-	assert.Equal(t, true, strings.Contains(actual, `Log/AppenderType,string,RollingFile`))
-	assert.Equal(t, true, strings.Contains(actual, `Log/RollingFile/MaximumFileSizeMB,string,50`))
-
-	assert.Equal(t, true, strings.Contains(actual, `Environment,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `AppClientId,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `WebApp/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Sentry/SentryDSN,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Log/RollingFile/File,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `ConsoleCredentials/Username,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `ConsoleCredentials/Password,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Redis/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Redis/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `Redis/Database,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Database,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/SecretKey,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/ClientSecret,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Port,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Database,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `ServiceEndpoints/Endpoint1/Endpoint,string,`))
-	assert.Equal(t, true, strings.Contains(actual, `ServiceEndpoints/Endpoint2/Endpoint,string,`))
+	assert.Equal(t, true, strings.Contains(actual, `Environment`))
+	assert.Equal(t, true, strings.Contains(actual, `AppClientId`))
+	assert.Equal(t, true, strings.Contains(actual, `WebApp/Port`))
+	assert.Equal(t, true, strings.Contains(actual, `Sentry/SentryDSN`))
+	assert.Equal(t, true, strings.Contains(actual, `Log/RollingFile/File`))
+	assert.Equal(t, true, strings.Contains(actual, `ConsoleCredentials/Username`))
+	assert.Equal(t, true, strings.Contains(actual, `ConsoleCredentials/Password`))
+	assert.Equal(t, true, strings.Contains(actual, `Redis/Endpoint`))
+	assert.Equal(t, true, strings.Contains(actual, `Redis/Port`))
+	assert.Equal(t, true, strings.Contains(actual, `Redis/Database`))
+	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Endpoint`))
+	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Port`))
+	assert.Equal(t, true, strings.Contains(actual, `AccountsRedis/Database`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/SecretKey`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/ClientSecret`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Endpoint`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Port`))
+	assert.Equal(t, true, strings.Contains(actual, `OAuth2/CacheRedis/Database`))
+	assert.Equal(t, true, strings.Contains(actual, `ServiceEndpoints/Endpoint1/Endpoint`))
+	assert.Equal(t, true, strings.Contains(actual, `ServiceEndpoints/Endpoint2/Endpoint`))
 }
